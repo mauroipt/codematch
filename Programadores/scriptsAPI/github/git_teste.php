048b14a0a40484536194a87e274626982e06f199
g-esteves <goncalo.e@gmail.com>
2011-03-17 19:36:00 +0000

    Bugs Corrigidos: - dentro do foreach: $json para $arg - melhorado aspecto gráfico - link do autor corrigido - tag <a href='...></a> faltava ' no final, ficando <a href='...'>Autor</a>

--- Programadores/scriptsAPI/github/git_teste.php
+++ Programadores/scriptsAPI/github/git_teste.php
@@ -1,16 +1,17 @@
 <?php
-
 	$ch = @curl_init();
-		curl_setopt($ch, CURLOPT_URL, "http://github.com/api/v2/json/repos/search/ruby+testing");
-		curl_setopt($ch, CURLOPT_USERAGENT, 'Googlebot/2.1');
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_URL, "http://github.com/api/v2/json/repos/search/ruby+testing");
+	curl_setopt($ch, CURLOPT_USERAGENT, 'Googlebot/2.1');
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	$page = curl_exec( $ch);
 	curl_close($ch);
 	//descodifica uma string json
 	$json = json_decode($page);
 
 	foreach($json->repositories as $arg) {
-		print_r("<b>Projecto:</b> ".$arg->name." - <b> Descricao: </b> ".$json->description." - <b>Autor:</b> <a href='https://bitbucket.org/".$json->username." .</a><br>");
-		print_r("&nbsp<b>Seguidores</b>: ".$json->followers."<b>Watchers: </b>".$json->watchers."<b>Criado a: </b>".$json->created_at."</a></br>");
+		print_r("<b>Projecto: </b>".$arg->name." - <b>Autor: </b><a href='https://bitbucket.org/".$arg->username."/'>".$arg->username."</a><br>");
+		print_r("<b>Descricao: </b>".$arg->description."<br>");
+		print_r("<b>Seguidores: </b>".$arg->followers." - <b>Watchers: </b>".$arg->watchers." - <b>Criado a: </b>".$arg->created_at."<br>");
+		print_r("<br><br>");
 	}	
 ?>
